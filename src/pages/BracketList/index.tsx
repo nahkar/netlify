79b466de429844dbe683311a2bd09746f03b4f67
Belenov Dmitriy <dimabelenov@gmail.com>
2023-09-27 00:00:32 +0300

    feature: add remove bracket logic

--- src/pages/BracketList/index.tsx
+++ src/pages/BracketList/index.tsx
@@ -1,13 +1,18 @@
 import { useBracketList } from './hooks/useBracketList';
 
 export const BracketList = () => {
-	const { brackets } = useBracketList();
+	const { brackets, openBracketHandler, isLoading, deleteBracketHandler } = useBracketList();
+	if (isLoading) return <div>Loading...</div>;
 	return (
 		<>
 			<div>BracketList</div>
 			<ul>
 				{brackets.map((bracket) => (
-					<li key={bracket.id}>{bracket.name}</li>
+					<li key={bracket.id}>
+						<span>{bracket.name}</span>
+						<button onClick={() => openBracketHandler(bracket.id)}>Open</button>
+						<button onClick={() => deleteBracketHandler(bracket.id)}>Remove</button>
+					</li>
 				))}
 			</ul>
 		</>
