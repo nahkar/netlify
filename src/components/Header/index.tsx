c6a6b7b3936e216bc45d132f3502651e8a7598c2
Belenov Dmitriy <dimabelenov@gmail.com>
2023-10-06 23:57:02 +0300

    feature: test

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -19,39 +19,39 @@ import CreateNewFolderIcon from '@mui/icons-material/CreateNewFolder';
 export const Header = () => {
 	const { isOpen, onOpen, onClose } = useHeader();
 	return (
-		<AppBar position='fixed' data-testid='header'>
+		<AppBar position="fixed" data-testid="header">
 			<Toolbar>
-				<IconButton size='large' edge='start' color='inherit' sx={{ mr: 2 }} onClick={onOpen} data-testid='user-menu'>
+				<IconButton size="large" edge="start" color="inherit" sx={{ mr: 2 }} onClick={onOpen} data-testid="user-menu">
 					<MenuIcon />
 				</IconButton>
 				<Drawer open={isOpen} onClose={onClose}>
-					<Box sx={{ width: 250 }} onClick={onClose} onKeyDown={onClose} data-testid='user-menu-list'>
+					<Box sx={{ width: 250 }} onClick={onClose} onKeyDown={onClose} data-testid="user-menu-list">
 						<List>
 							<ListItem disablePadding sx={{ display: 'block' }}>
-								<Header__LinkMenuStyled to='create-bracket'>
+								<Header__LinkMenuStyled to="create-bracket">
 									<ListItemButton>
 										<ListItemIcon>
-											<CreateNewFolderIcon color='info' />
+											<CreateNewFolderIcon color="info" />
 										</ListItemIcon>
-										<ListItemText primary='Create Bracket' />
+										<ListItemText primary="Create Bracket" />
 									</ListItemButton>
 								</Header__LinkMenuStyled>
 							</ListItem>
 							<ListItem disablePadding sx={{ display: 'block' }}>
-								<Header__LinkMenuStyled to='brackets'>
+								<Header__LinkMenuStyled to="brackets">
 									<ListItemButton>
 										<ListItemIcon>
-											<ViewListIcon color='info' />
+											<ViewListIcon color="info" />
 										</ListItemIcon>
-										<ListItemText primary='My Brackets'/>
+										<ListItemText primary="My Brackets" />
 									</ListItemButton>
 								</Header__LinkMenuStyled>
 							</ListItem>
 						</List>
 					</Box>
 				</Drawer>
-				<Typography variant='h6' component='div' sx={{ flexGrow: 1 }} align='center'>
-					<Header__LinkStyled to='/'>Bracket Builder</Header__LinkStyled>
+				<Typography variant="h6" component="div" sx={{ flexGrow: 1 }} align="center">
+					<Header__LinkStyled to="/">Bracket Builder</Header__LinkStyled>
 				</Typography>
 			</Toolbar>
 		</AppBar>
