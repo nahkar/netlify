341732d3ef207beebeb99cbdfa2c7f01319ad54c
Belenov Dmitriy <dimabelenov@gmail.com>
2023-10-06 22:16:03 +0300

    feature: test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -17,7 +17,7 @@ module.exports = {
 		project: true,
 		tsconfigRootDir: __dirname,
 	},
-	plugins: ['react-refresh'],
+	plugins: ['react-refresh','react', '@typescript-eslint', 'prettier'],
 	rules: {
 		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
 		'@typescript-eslint/no-non-null-assertion': 'off',
