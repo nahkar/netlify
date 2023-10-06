3bdf3429e5f90f4769e069dbb262c65872416338
Belenov Dmitriy <dimabelenov@gmail.com>
2023-10-06 20:12:12 +0300

    feature: add husky

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,10 @@
 
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import EnvironmentPlugin from 'vite-plugin-environment';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), EnvironmentPlugin('all')],
+	plugins: [react()],
   test: {
     globals: true,
     environment: 'jsdom',
