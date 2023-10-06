8ccb57dc8c2b8713fb5ec711104c6f549e443f5e
Belenov Dmitriy <dimabelenov@gmail.com>
2023-10-06 22:06:48 +0300

    feature: test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,32 +1,30 @@
 /* eslint-env node */
 
 module.exports = {
-  root: true,
-  env: {
-    browser: true,
-    es2020: true,
-  },
-  extends: [
-    'eslint:recommended',
+	root: true,
+	env: { browser: true, es2020: true },
+	extends: [
+		'eslint:recommended',
     'plugin:react/recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@typescript-eslint/recommended-requiring-type-checking',
     'plugin:react-hooks/recommended',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 2020, // Set to the appropriate ECMAScript version
-    sourceType: 'module',
-    project: ['./tsconfig.json'], // Specify your tsconfig.json path
-  },
-  plugins: ['react', '@typescript-eslint', 'react-hooks'],
-  rules: {
-    // Add your project-specific ESLint rules here
-  },
-  settings: {
-    react: {
-      version: 'detect', // Automatically detect the React version
-    },
-  },
-  ignorePatterns: ['.eslintrc.cjs'],
+	],
+	parser: '@typescript-eslint/parser',
+	parserOptions: {
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: true,
+		tsconfigRootDir: __dirname,
+	},
+	plugins: ['react-refresh'],
+	rules: {
+		'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+		'@typescript-eslint/no-non-null-assertion': 'off',
+		'@typescript-eslint/no-unsafe-assignment': 'off',
+		'@typescript-eslint/no-unsafe-call': 'off',
+		'@typescript-eslint/no-explicit-any': 'off',
+		'@typescript-eslint/no-unsafe-member-access': 'off',
+	},
+	ignorePatterns: ['.eslintrc.cjs'],
 };
